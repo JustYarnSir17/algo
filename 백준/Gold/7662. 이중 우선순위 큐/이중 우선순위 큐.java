da6595dc410b2b6a0bf390aef7349a0f1f26008a
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2023-02-22 16:32:18 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2532 ms, Memory: 445584 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -8,27 +8,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-	static int del(PriorityQueue<Integer> q, Map<Integer, Integer> map) {
-		int num;
-		while (true) {
-			num = q.poll();
-			int cnt = map.getOrDefault(num, 0);
-			if (cnt == 0)
-				continue;
-
-			if (cnt == 1)
-				map.remove(num);
-
-			else
-				map.put(num, cnt - 1);
-
-			break;
-		}
-		return num;
-
-	}
 
 	public static void main(String[] args) throws IOException {
 
@@ -36,40 +18,30 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
-
-			PriorityQueue<Integer> minq = new PriorityQueue<>();
-			PriorityQueue<Integer> maxq = new PriorityQueue<>(Collections.reverseOrder());
-			Map<Integer, Integer> map = new HashMap<>();
 			int n = Integer.parseInt(br.readLine());
+			TreeMap<Integer, Integer> q = new TreeMap<>();
 			for (int i = 0; i < n; i++) {
 				String str = br.readLine();
 				StringTokenizer st = new StringTokenizer(str, " ");
 				String a = st.nextToken();
 				int b = Integer.parseInt(st.nextToken());
 				if (a.equals("I")) {
-					minq.add(b);
-					maxq.add(b);
-					
-					map.put(b, map.getOrDefault(b, 0) + 1);
+					q.put(b, q.getOrDefault(b, 0) + 1);
 
 				} else {
-					if (map.size() == 0) {
+					if (q.size() == 0) {
 						continue;
 					}
-					PriorityQueue<Integer> q = b == 1 ? maxq : minq;
-					del(q, map);
+					int tmp =b == 1 ? q.lastKey() : q.firstKey();
+					if (q.put(tmp, q.get(tmp) - 1) == 1) {
+						q.remove(tmp);
+					}
 
 				}
 
 			}
-			if (map.size() == 0) {
-				bw.write("EMPTY");
-				bw.newLine();
-			} else {
-				int tmp = del(maxq, map);
-				bw.write(tmp + " " + (map.size() > 0 ? del(minq, map) : tmp));
-				bw.newLine();
-			}
+			bw.write(q.size()==0?"EMPTY":q.lastKey()+" "+q.firstKey());
+			bw.newLine();
 
 		}
 
