c9123c2d495fa16cc6f01cb554446af59ef616fa
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2023-02-26 14:12:35 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 1004 ms, Memory: 92076 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	static ArrayList<Integer>[] list;
+	static int n;
+	static int m;
+	static int r;
+	static int[]v;
+	static void dfs(int r,int d) {
+		v[r]=d;
+		for(int i=list[r].size()-1;i>=0;i--) {
+			if(v[list[r].get(i)]==-1) {
+				dfs(list[r].get(i),d+1);
+				
+			}
+				
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		n =Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		list= new ArrayList[n+1];
+		v=new int[n+1];
+		for(int i=0;i<n+1;i++) {
+			list[i]=new ArrayList<Integer>();
+			v[i]=-1;
+		}
+		for(int i=0;i<m;i++) {
+			str = br.readLine();
+			st= new StringTokenizer(str," ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			list[x].add(y);
+			list[y].add(x);
+		}
+		for(int i=1;i<=n;i++) {
+			Collections.sort(list[i]);
+		}
+		dfs(r,0);
+		
+		for(int i=1;i<=n;i++) {
+			bw.write(String.valueOf(v[i]));
+			bw.newLine();
+		}
+
+		bw.close();
+		br.close();
+	}
+}
