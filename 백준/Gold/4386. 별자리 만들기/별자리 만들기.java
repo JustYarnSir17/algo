1941b545dbf809c51fd00de91406e6dff29dccda
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2023-03-29 17:37:29 +0900

    [Gold III] Title: 별자리 만들기, Time: 92 ms, Memory: 12332 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] p;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n =Integer.parseInt(br.readLine());
+		p=new int [n+1];
+		double [][]v=new double [n][2];
+		int num= n*(n-1)/2;
+		for(int i=0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			v[i][0]=Double.parseDouble(st.nextToken());
+			v[i][1]=Double.parseDouble(st.nextToken());
+		}
+		double [][]edges= new double [num][3];
+		int idx=0;
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				edges[idx][0]=i;
+				edges[idx][1]=j;
+				edges[idx++][2]= Math.sqrt(Math.pow(v[i][0]-v[j][0], 2)+Math.pow(v[i][1]-v[j][1], 2));
+				
+			}
+		}
+		for(int i=0;i<=n;i++) {
+			p[i]=i;
+		}
+		Arrays.sort(edges,new Comparator<double[]>() {
+			@Override
+			public int compare(double[] o1, double[] o2) {
+				// TODO Auto-generated method stub
+				if(o1[2]-o2[2]<0)
+					return -1;
+				else if(o1[2]-o2[2]==0)
+					return 0;
+				else 
+					return 1;
+			}
+		});
+		double result=0;
+		int cnt=0;
+		for(int i=0;i<num;i++) {
+			if(findset((int)edges[i][0])!=findset((int)edges[i][1])) {
+				union((int)edges[i][0],(int)edges[i][1]);
+				result+=edges[i][2];
+				cnt++;
+			}
+			if(cnt==n-1)
+				break;
+		}
+		result = Math.round(result*100)/100.0;
+		bw.write(String.valueOf(result));
+		bw.close();
+		br.close();
+	}
+	private static void union(int i, int j) {
+		// TODO Auto-generated method stub
+		p[findset(i)]=findset(j);
+	}
+	private static int findset(int i) {
+		// TODO Auto-generated method stub
+		if(p[i]==i)return i;
+		p[i]=findset(p[i]);
+		return p[i];
+	}
+}
