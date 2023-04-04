d559e0ad7b222cb161ce81217565781094ef14d9
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2023-04-04 11:07:28 +0900

    [Gold V] Title: 탑, Time: 780 ms, Memory: 158072 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] arr;
+	static Stack<Integer> stack = new Stack<>();
+	static int[] sig;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[n + 1];
+		sig = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = n; i >= 1; i--) {
+			if (stack.isEmpty()) {
+				stack.add(i);
+			} else {
+				while (arr[i] > arr[stack.peek()]) {
+					sig[stack.pop()] = i;
+					if(stack.isEmpty())
+						break;
+				}
+				stack.add(i);
+			}
+		}
+		while (!stack.isEmpty()) {
+			sig[stack.pop()] = 0;
+		}
+		for (int i = 1; i <= n; i++) {
+			bw.write(sig[i] + " ");
+		}
+		bw.close();
+		br.close();
+	}
+}
