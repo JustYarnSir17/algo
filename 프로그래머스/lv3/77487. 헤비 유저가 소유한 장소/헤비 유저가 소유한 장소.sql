c49e38eb6e405b04d4a9d98885ebca666999296a
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2023-09-14 13:36:14 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,4 +1,2 @@
 -- 코드를 입력하세요
-SELECT ID,NAME,HOST_ID FROM PLACES
-WHERE host_id in(select host_id from places group by host_id having count(*)>1)
-order by id asc
+SELECT * FROM PLACES where host_id in  (select host_id from places group by host_id having count(*)>=2) order by id
