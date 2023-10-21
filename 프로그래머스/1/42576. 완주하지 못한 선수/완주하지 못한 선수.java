4b6c09149170156010afc44d59db98b9f7e32330
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2023-10-22 02:33:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.11 ms, Memory: 96.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String,Integer> map = new HashMap<>();
+        String [] arr = new String [participant.length];
+        String answer = "";
+        for(int i=0;i<participant.length;i++){
+            if(map.containsKey(participant[i])){
+                map.replace(participant[i],map.get(participant[i])+1);
+            }
+            else{
+            map.put(participant[i],1);}
+            arr[i]=participant[i];
+        }
+        for(int i=0;i<completion.length;i++){
+            if(map.get(completion[i])==1){
+                map.remove(completion[i]);
+            }
+            else{
+                map.replace(completion[i],map.get(completion[i])-1);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                answer=arr[i];
+                break;
+            }
+        }
+        
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
