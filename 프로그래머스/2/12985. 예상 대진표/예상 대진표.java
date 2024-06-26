f62d3cf87af8953c24a4e2e5e34cc395695d159c
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-06-26 13:37:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(a!=b){
+            ++answer;
+            if(a%2==0){
+                a=a/2;
+            }
+            else{
+                a=a/2+1;
+            }
+            if(b%2==0){
+                b=b/2;
+            }
+            else{
+                b=b/2+1;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
