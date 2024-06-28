5cd44d499a8c3a12bee3b936830d488f4444078e
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-06-28 21:09:55 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 13.01 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -2,43 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        Stack<Pair> stack = new Stack<>();
-        
-        
-        
         int[] answer = new int[numbers.length];
+        Arrays.fill(answer,-1);
         
-        for(int i=0;i<numbers.length;i++){
-            if(stack.isEmpty()){
-                stack.push(new Pair(i,numbers[i]));
-                
-            }
-            else{
-                while(stack.peek().val<numbers[i]){
-                    Pair tmp = stack.pop();
-                    answer[tmp.num]=numbers[i];
-                    if(stack.isEmpty()){
+        for(int i= numbers.length-2;i>=0;i--){
+            for(int j=i+1;j<numbers.length;j++ ){
+                if(numbers[i]<numbers[j]){
+                    answer[i]=numbers[j];
+                    break;
+                }
+                else{
+                    if(answer[j]==-1){
                         break;
                     }
+                    else if(numbers[i]<answer[j]){
+                        answer[i]=answer[j];
+                        break;
+                    }
+                    
                 }
-                stack.push(new Pair(i,numbers[i]));
             }
-        }
-        while(!stack.isEmpty()){
-            Pair tmp = stack.pop();
-            answer[tmp.num]=-1;
-        }
-        
+            }
         return answer;
     }
     
-}
-
-class Pair{
-    int num;
-    int val;
-    Pair(int num,int val){
-        this.num=num;
-        this.val=val;
-    }
 }
