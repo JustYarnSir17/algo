a5e80297bea3af1973e4df4532f91efe884c2b19
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-06-28 22:51:30 +0900

    [level 2] Title: 가장 큰 수, Time: 1.80 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = Integer.toString(numbers[i]);
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String order1 = o1 + o2;
+                String order2 = o2 + o1;
+                return order2.compareTo(order1); 
+            }
+        });
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (String num : arr) {
+            answer.append(num);
+        }
+
+        return answer.toString();
+    }
+}
