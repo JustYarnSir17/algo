d20199f9e59449f57bf787c15a4f1b6496f38019
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-07-14 07:35:45 +0900

    [Gold IV] Title: 플로이드, Time: 320 ms, Memory: 42384 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int [][] arr = new int[n][n];
+        int INF = 100000001;
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(arr[i], INF);
+            arr[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            arr[a][b] = Math.min(arr[a][b], c);  
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[i][k] != INF && arr[k][j] != INF) {
+                        arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == INF) {
+                    bw.write("0 ");
+                } else {
+                    bw.write(arr[i][j] + " ");
+                }
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
