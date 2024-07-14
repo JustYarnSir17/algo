dd0d346c5f35fa8cb36e46a786083742d2cca73a
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-07-15 07:11:30 +0900

    [Gold I] Title: 최종 순위, Time: 320 ms, Memory: 41400 KB -BaekjoonHub

--- 백준/Gold/3665. 최종 순위/최종 순위.java
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -4,85 +4,82 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            int[] inDegree = new int[n + 1];
-            boolean[][] adj = new boolean[n + 1][n + 1];
-            int[] lastYear = new int[n];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            for (int i = 0; i < n; i++) {
-                lastYear[i] = Integer.parseInt(st.nextToken());
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    adj[lastYear[i]][lastYear[j]] = true;
-                    inDegree[lastYear[j]]++;
-                }
-            }
-
-            int m = Integer.parseInt(br.readLine());
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                if (adj[a][b]) {
-                    adj[a][b] = false;
-                    adj[b][a] = true;
-                    inDegree[a]++;
-                    inDegree[b]--;
-                } else {
-                    adj[a][b] = true;
-                    adj[b][a] = false;
-                    inDegree[a]--;
-                    inDegree[b]++;
-                }
-            }
-
-            Queue<Integer> queue = new LinkedList<>();
-            for (int i = 1; i <= n; i++) {
-                if (inDegree[i] == 0) {
-                    queue.add(i);
-                }
-            }
-
-            List<Integer> result = new ArrayList<>();
-            boolean isDetermined = true;
-
-            for (int i = 0; i < n; i++) {
-                if (queue.isEmpty()) {
-                    isDetermined = false;
-                    break;
-                }
-                if (queue.size() > 1) {
-                    isDetermined = false;
-                    break;
-                }
-
-                int curr = queue.poll();
-                result.add(curr);
-                for (int j = 1; j <= n; j++) {
-                    if (adj[curr][j]) {
-                        inDegree[j]--;
-                        if (inDegree[j] == 0) {
-                            queue.add(j);
-                        }
-                    }
-                }
-            }
-
-            if (!isDetermined) {
-                System.out.println("IMPOSSIBLE");
-            } else {
-                for (int team : result) {
-                    System.out.print(team + " ");
-                }
-                System.out.println();
-            }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while(T-->0) {
+        	int n=Integer.parseInt(br.readLine());
+        	int [] last = new int [n];// 작년 순위
+        	int [] in = new int[n+1];//진입 배열
+        	boolean [][] arr = new boolean [n+1][n+1]; // 상대 순위 비교 
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	for(int i=0;i<n;i++) {
+        		int tmp = Integer.parseInt(st.nextToken());
+        		last[i]=tmp;
+        	}
+        	for(int i=0;i<n;i++) {
+        		for(int j=i+1;j<n;j++) {
+        			arr[last[i]][last[j]]=true;//last[i]가 last[j]보다 우선한다.
+        			in[last[j]]++; //따라서 last[j]의 진입 순번 증가 
+        		}
+        	}
+        	int m=Integer.parseInt(br.readLine());
+        	for(int i=0;i<m;i++) {
+        		st= new StringTokenizer(br.readLine());
+        		int a=Integer.parseInt(st.nextToken());
+        		int b=Integer.parseInt(st.nextToken());
+        		if(arr[a][b]) {//순서 변화
+        			arr[a][b]=false;
+        			arr[b][a]=true;
+            		in[a]++;
+            		in[b]--;
+        		}
+        		else {//순서 변화
+        			arr[a][b]=true;
+        			arr[b][a]=false;
+        			in[a]--;
+        			in[b]++;
+        		}
+        	}
+        	boolean tr =true;
+        	int[] result = new int [n];
+        	int  idx=0;
+        	Queue<Integer> q = new LinkedList<>();
+        	for(int i=1;i<=n;i++) {
+        		if(in[i]==0) {
+        			q.offer(i);
+        		}
+        	}
+        	for(int i=0;i<n;i++) {
+        		if(q.isEmpty()) {
+        			tr=false;
+        			break;
+        		}
+        		if(q.size()>1) {
+        			tr=false;
+        			break;
+        		}
+        		int curr= q.poll();
+        		result[idx++]=curr;
+        		for(int j=1;j<=n;j++) {
+        			if(arr[curr][j]) {
+        				if(--in[j]==0) {
+        					q.offer(j);
+        				}
+        				
+        			}
+        		}
+        	}
+        	if(!tr) {
+        		bw.write("IMPOSSIBLE\n");
+        	}
+        	else {
+        		for(int i=0;i<n;i++) {
+        			bw.write(result[i]+" ");
+        		}
+        		bw.newLine();
+        	}
         }
+        bw.close();
+        br.close();
     }
 }
