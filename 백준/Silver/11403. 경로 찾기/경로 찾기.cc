faeda3f0460951d2586d6462ca7da9be9c1f4993
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-07-16 08:04:09 +0900

    [Silver I] Title: 경로 찾기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<vector<int>> arr(n,vector<int>(n));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][k] == 1 && arr[k][j] == 1) {
+					arr[i][j] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
