659fa7400c1c1e5777676546d62f8fe440f6f386
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-08-22 10:42:43 +0900

    [Gold IV] Title: 전화번호 목록, Time: 264 ms, Memory: 100524 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,38 +1,76 @@
 import java.io.*;
 import java.util.*;
 
+
+
 public class Main {
+	static HashSet<Integer> set =new HashSet<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int T = Integer.parseInt(br.readLine());
-        for(int Time = 0; Time < T; Time++) {
-            int n = Integer.parseInt(br.readLine());
-            String[] numbers = new String[n];
-            for (int i = 0; i < n; i++) {
-                numbers[i] = br.readLine();
-            }
-            
-            // 전화번호를 사전순으로 정렬
-            Arrays.sort(numbers);
-            
-            boolean isConsistent = true;
-            for (int i = 0; i < n - 1; i++) {
-                // 인접한 두 전화번호가 접두사 관계인지 확인
-                if (numbers[i + 1].startsWith(numbers[i])) {//startWith는 특정 문자로 시작하는지 체크하는 매서드
-                    isConsistent = false;
-                    break;
-                }
-            }
-            
-            if (isConsistent) {
-                bw.write("YES\n");
-            } else {
-                bw.write("NO\n");
-            }
+        int Time= Integer.parseInt(br.readLine());
+        for(int t=0;t<Time;t++) {
+        	Trie trie=new Trie();
+        	int n =Integer.parseInt(br.readLine());
+        	boolean answer=false;
+        	for(int i=0;i<n;i++) {
+        		String str = br.readLine();
+        		
+        		if(!answer) {
+        			answer= trie.insert(str);
+        		}
+        	}
+        	if(answer) {
+        		bw.write("NO\n");
+        		
+        	}
+        	else {
+        		bw.write("YES\n");
+        	}
+        	
+        	
         }
         bw.close();
         br.close();
+        
     }
+}
+
+class TrieNode{
+	TrieNode[] children = new TrieNode[10];
+	boolean isEnd= false;
+}
+
+class Trie{
+	private TrieNode root;
+	
+	public Trie() {
+		root= new TrieNode();
+		
+	}
+	
+	public boolean insert(String word) {
+		boolean answer= false;
+		TrieNode node = root;
+		for(int i=0;i<word.length();i++) {
+			int idx = word.charAt(i)-'0';
+			if(node.children[idx]==null) {
+				node.children[idx]=new TrieNode();
+			}
+			node = node.children[idx];
+			if(node.isEnd) {//기존 번호의 접두어인 경우
+				answer=true;
+			}
+		}
+		for(TrieNode child:node.children) {//기존 번호가 이 번호의 접두어인 경우
+			if(child!=null) {
+				return true;
+			}
+		}
+		
+		node.isEnd=true;
+		return answer;
+	}
+	
 }
