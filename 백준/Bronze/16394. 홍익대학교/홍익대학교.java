31aef51c97872d09a8efe7b61939cb41634c0b5b
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-08-25 23:56:37 +0900

    [Bronze V] Title: 홍익대학교, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(br.readLine());
+        
+        bw.write(String.valueOf(n-1946));
+        bw.close();
+        br.close();
+    }
+    
+    
+}
