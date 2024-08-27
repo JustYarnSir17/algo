6ea39efe6338ee61f759e11ecd5b24ea52333cdd
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-08-28 07:50:05 +0900

    [Gold IV] Title: 사탕 가게, Time: 1252 ms, Memory: 39588 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -16,7 +16,10 @@ public class Main {
         		st=new StringTokenizer(br.readLine());
         		int c=Integer.parseInt(st.nextToken());
         		double p =Double.parseDouble(st.nextToken());
-        		int price= (int)(p*100+0.5);
+        		int price= (int)(p*100+0.5);//0.5를 더해주고 int로 형변환을 해주는 이유는 Double의 경우 부동소수점 자료형이기 때문에
+        		//표시되는 값과 실제로 인식되는 값의 차이가 있을 수 있다. 이는 컴퓨터가 2진수 체계로 이뤄져있기에 10진수의 소수점을 정확하게 표현하기 힘든 것에서 기인한다.
+        		//따라서 0.5를 더해서 실제로는 0.XXX 부족하게 처리되는 수를 확실하게 Int로 만들고 소수점 아래는 버리기 위함이다.
+        	
         		candy[i][0]=c;
         		candy[i][1]=price;
         	}
