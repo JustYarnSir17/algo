d020cb933164e1fe84c768c543d861c0e32dd9dc
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-10-10 22:00:24 +0900

    [Gold V] Title: 퇴사 2, Time: 688 ms, Memory: 343644 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n =Integer.parseInt(br.readLine());
+        int [][]arr = new int [n][2];
+        int [] dp= new int[n+1];
+        for(int i=0;i<n;i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int t = Integer.parseInt(st.nextToken());
+        	int p = Integer.parseInt(st.nextToken());
+        	arr[i][0]=t;
+        	arr[i][1]=p;	
+        }
+        
+		for(int i=n-1;i>=0;i--) {
+			if(i+arr[i][0]<=n) {
+				dp[i]=Math.max(dp[i+1],arr[i][1]+dp[i+arr[i][0]]);
+			}
+			else {
+				dp[i]=dp[i+1];
+			}
+		}
+		bw.write(dp[0]+"");
+		bw.close();
+		br.close();
+	}
+	
+}
