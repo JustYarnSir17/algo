533cf151a0a5f1eb7d41b155ba1103f2cc948d17
JustYarnSir17 <122420033+JustYarnSir17@users.noreply.github.com>
2024-12-04 20:47:25 +0900

    [level 3] Title: 이중우선순위큐, Time: 40.10 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,93 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        PriorityQueue<Node> minPq = new PriorityQueue<>(); // 기본적으로 오름차순
-        PriorityQueue<Node> maxPq = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o2.val - o1.val; // 내림차순
+        TreeMap<Integer,Integer> treeMap =new TreeMap<>();
+        int idx=0;
+        for(int i=0;i<operations.length;i++){
+            StringTokenizer st = new StringTokenizer(operations[i]);
+            String com = st.nextToken();
+            if(com.equals("I")){
+                int val =Integer.parseInt(st.nextToken());
+                treeMap.put(val,idx++);
             }
-        });
-
-        int idx = 0;
-        HashSet<Integer> set = new HashSet<>();
-
-        for (String operation : operations) {
-            StringTokenizer st = new StringTokenizer(operation);
-            String command = st.nextToken();
-            if (command.equals("I")) {
+            else{
                 int val = Integer.parseInt(st.nextToken());
-                Node node = new Node(idx, val);
-                minPq.offer(node);
-                maxPq.offer(node);
-                set.add(idx++);
-            } else {
-                int val = Integer.parseInt(st.nextToken());
-                if (val == 1) {
-                    Node tmp = maxPq.poll();
-                    while (tmp != null && !set.contains(tmp.idx)) {
-                        tmp = maxPq.poll();
+                if(val==1){
+                    if(!treeMap.isEmpty()){
+                        treeMap.pollLastEntry();
                     }
-                    if (tmp != null) {
-                        set.remove(tmp.idx);
-                    }
-                } else {
-                    Node tmp = minPq.poll();
-                    while (tmp != null && !set.contains(tmp.idx)) {
-                        tmp = minPq.poll();
-                    }
-                    if (tmp != null) {
-                        set.remove(tmp.idx);
+                }
+                else{
+                    if(!treeMap.isEmpty()){
+                        treeMap.pollFirstEntry();
                     }
                 }
             }
         }
-
-        if (set.isEmpty()) {
-            answer[0] = 0;
-            answer[1] = 0;
-        } else {
-            Node low = null;
-            Node high = null;
-
-            while (!minPq.isEmpty()) {
-                Node tmp = minPq.poll();
-                if (set.contains(tmp.idx)) {
-                    low = tmp;
-                    break;
-                }
-            }
-
-            while (!maxPq.isEmpty()) {
-                Node tmp = maxPq.poll();
-                if (set.contains(tmp.idx)) {
-                    high = tmp;
-                    break;
-                }
-            }
-
-            if (low != null && high != null) {
-                answer[0] = high.val;
-                answer[1] = low.val;
-            }
+        
+        if(treeMap.isEmpty()){
+            return new int[]{0,0};
         }
-
-        return answer;
-    }
-}
-
-class Node implements Comparable<Node> {
-    int idx;
-    int val;
-
-    Node(int idx, int val) {
-        this.idx = idx;
-        this.val = val;
-    }
-
-    @Override
-    public int compareTo(Node other) {
-        return Integer.compare(this.val, other.val);
+        else{
+            int [] answer= new int[2];
+            answer[0]=treeMap.lastKey();
+            answer[1]=treeMap.firstKey();
+            return answer;
+        }
     }
 }
